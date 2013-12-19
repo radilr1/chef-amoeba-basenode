7920fcef09ebeb7cb9409c6f0b80be85b2e5b05d
Daniel Jabbour <daniel@amoe.ba>
2013-12-18 20:37:46 -0800

    Fixing bug in node deps

--- recipes/deployer.rb
+++ recipes/deployer.rb
@@ -2,7 +2,7 @@
 # specify values for "node.deployment" values. Here we look at the providers databag and try to
 # fetch default values for node.deployment.
 #
-if node.deployment.provider
+if node[:deployment][:provider]
   node.default.deployment = data_bag_item('providers', node.deployment.provider).raw_data
 end
 
