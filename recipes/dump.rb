ff74aa77219eaa1739a85f85d0333ebe9a6e9771
Daniel Jabbour <daniel@amoe.ba>
2014-01-16 04:41:51 -0800

    Removing ident from node data during dump

--- recipes/dump.rb
+++ recipes/dump.rb
@@ -3,9 +3,15 @@ node_filename = "#{node[:deployment][:home]}/node.json"
 log "Dumping node definition to: [#{node_filename}]"
 ruby_block "dump_node_json" do
   block do
+    # for_json contains the individual default / automatic sections. to_hash is merged / flat
+    node_hash = JSON.parse(JSON(node.to_hash))
+
+    # Remove ident as host keys will be managed by us and it's not useful anymore
+    node_hash['deployment'].delete('ident')
+
     File.open(node_filename, 'w') do |f|
-      f.write(JSON(node.to_hash.merge(node.for_json)))
-      f.chmod(0640)
+      f.write( JSON(node_hash) )
+      f.chmod( 0640 )
     end
     FileUtils.chown node.deployment.user, node.deployment.group, node_filename
   end
