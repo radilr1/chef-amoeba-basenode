5d2089a0ffb81a1e5e22682a94396c5cebe8b14b
radilr <radil@fandrop.com>
2014-05-09 17:48:06 +0300

    fix

--- definitions/authorized_keys.rb
+++ definitions/authorized_keys.rb
@@ -14,13 +14,12 @@ define :authorized_keys, user: nil, env: "development", home: nil do
   # load authorized_keys data_bag for password-less login
   pubkeys = []
   key_users = data_bag 'authorized_keys'
-  if key_users.include? user_name
-    #data_bag_item('authorized_keys', user_name).include?(env)
+  if key_users.include? user_name && data_bag_item('authorized_keys', user_name)[env]
     pubkeys += data_bag_item('authorized_keys', user_name)[env]
   end
 
   # 'root' keys are applied to all users, not just for root
-  if user_name != 'root' and key_users.include? 'root'
+  if user_name != 'root' and key_users.include? 'root' && data_bag_item('authorized_keys', 'root')[app.name]
     pubkeys += data_bag_item('authorized_keys', 'root')[app.name]
   end
 
